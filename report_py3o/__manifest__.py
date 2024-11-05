0ada62595de6da3d870125154e122a20bd19ca3e
psugne <ugne.piscikaite@gmail.com>
2024-11-05 18:16:49 +0200

    [IMP] report_py3o: ignore libreoffice dependency

--- report_py3o/__manifest__.py
+++ report_py3o/__manifest__.py
@@ -12,7 +12,7 @@
     "depends": ["web"],
     "external_dependencies": {
         "python": ["py3o.template", "py3o.formats"],
-        "bin": ["libreoffice"],
+        "deb": ["libreoffice"],
     },
     "assets": {
         "web.assets_backend": [
